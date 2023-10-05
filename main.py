314fb51e22095ed7cd3da5eb69a88910764d51c8
JaneQPublic <147025023+JaneQPublic@users.noreply.github.com>
2023-10-05 14:24:12 +0800

    创建主窗口

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
-import dpage, epage, bfdepage
+import sys
+import dpage
+import epage
+import bfdepage
 
+# 定义主页界面
 class Ui_MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):  # 封装类函数用以窗口跳转
-        super(Ui_MainWindow, self).__init__()  # Ui——MainWindow 为主窗口类函数
+    # 封装类函数用以窗口跳转
+    def __init__(self):  
+        super(Ui_MainWindow, self).__init__()
         self.setupUi(self)
         self.retranslateUi(self)
+    
     def setupUi(self, MainWindow):
+        # 设置主窗口基本设定
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(620, 436)
         MainWindow.setWindowIcon(QtGui.QIcon('./img/b.jpg'))
         MainWindow.setStyleSheet("#MainWindow{border-image:url(./img/b.jpg)}")
+        
         self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        
+        # 设置主窗口内容
         self.title = QtWidgets.QLabel(parent=self.centralwidget)
         self.title.setGeometry(QtCore.QRect(280, 0, 301, 101))
         self.title.setTextFormat(QtCore.Qt.TextFormat.AutoText)
@@ -23,10 +33,13 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.title_w.setGeometry(QtCore.QRect(-55, 10, 441, 71))
         self.title_w.setObjectName("title_w")
         MainWindow.setCentralWidget(self.centralwidget)
+
+        # 创建工具栏
         self.toolBar = QtWidgets.QToolBar(parent=MainWindow)
         self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.ToolBarArea.TopToolBarArea, self.toolBar)
-        # 创建工具栏的action
+        
+        # 创建action
         self.actione = QtGui.QAction(parent=MainWindow)
         self.actione.setObjectName("actione")
         self.actione.triggered.connect(self.trans_E)
@@ -36,15 +49,17 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.action_show = QtGui.QAction(parent=MainWindow)
         self.action_show.setObjectName("action_show")
         self.action_show.triggered.connect(self.trans_bf)
+        
         # 工具栏加入三个action
         self.toolBar.addAction(self.actione)
         self.toolBar.addAction(self.actiond)
         self.toolBar.addAction(self.action_show)
-
+        
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
+        # 设置显示的内容
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "主页"))
         self.title.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:72pt; font-weight:700;\">S-DES</span></p></body></html>"))
@@ -54,20 +69,22 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.actiond.setText(_translate("MainWindow", "解密"))
         self.action_show.setText(_translate("MainWindow", "暴力破解"))
 
-    def trans_D(self):  # 子窗口跳转函数
+    def trans_D(self):  
+        # 解密窗口跳转
         self.ddd = dpage.Ui_Formd()
         self.ddd.show()
 
-    def trans_E(self):  # 子窗口跳转函数
+    def trans_E(self):  
+        # 加密窗口跳转
         self.eee = epage.Ui_Forme()
         self.eee.show()
 
     def trans_bf(self):
+        # 暴力破解窗口跳转
         self.bf = bfdepage.Ui_Formbf()
         self.bf.show()
 
 if __name__ == '__main__':
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     #实例化主窗口
     MainWindow = QtWidgets.QMainWindow()
